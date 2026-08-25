86d7a09266b1f40830e42d13957abf15068cfb6d
Priyanshudotdev <priyanshu.kayarkar009@gmail.com>
2026-08-25 17:42:14 +0530

    feat: add user cache helper functions with 60s TTL

--- src/redis.ts
+++ src/redis.ts
@@ -1,4 +1,5 @@
 import { createClient } from "redis";
+import { IUser } from "./schema.js";
 
 export const createRedisClient = async () => {
     const client = await createClient()
@@ -6,4 +7,33 @@ export const createRedisClient = async () => {
     .connect();
 
     return client;
+}
+
+
+const client = createRedisClient();
+
+export const cacheUser = async (userId:string, user:IUser) => {
+    if(!userId) return null;
+
+    await (await client).SET(`user:${userId}`, JSON.stringify(user));
+    await (await client).EXPIRE(`user:${userId}`, 60);
+    
+    return true;
+}
+
+export const getCachedUser = async(userId:string) => {
+    if(!userId) return null;
+
+    const rawCachedUser = await (await client).GET(`user:${userId}`);
+    if(!rawCachedUser) return null;
+
+    const cachedUser = JSON.parse(rawCachedUser);
+    return cachedUser;
+}
+
+export const deleteCachedUser = async(userId:string) => {
+    if(!userId) return null;
+
+    const cachedUser = await (await client).DEL(`user:${userId}`);
+    return cachedUser;
 }
