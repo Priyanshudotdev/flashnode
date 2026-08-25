2b476a270d9635aba91d77794f3f36db4327cc07
Priyanshudotdev <priyanshu.kayarkar009@gmail.com>
2026-08-25 19:51:42 +0530

    feat: support route-specific cache key prefixes in rate limiter middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,20 @@
 import type { NextFunction, Request, Response } from "express";
 import { createRateLimitCache, getRateLimitCache, increamenttRateLimitCache } from "./redis.js";
 
-export const rateLimiter = async (rateLimit:number, time:number) => {
+export const rateLimiter = (cacheKey:string, rateLimit:number, time:number) => {
     return async (req: Request, res:Response, next:NextFunction) => {
         const ip = req.ip;
-
+        const cacheKeyWithIp = `${cacheKey}${ip}`;
+        
         if(ip){
             // check if the user is in the cache or not
-            const cacheRateLimit = await getRateLimitCache(ip);
+            const cacheRateLimit = await getRateLimitCache(cacheKeyWithIp);
             if(!cacheRateLimit) {
-                await createRateLimitCache(ip, time);
+                await createRateLimitCache(cacheKeyWithIp,time);
                 next();
                 return;
             }
-            const newRateLimit = await increamenttRateLimitCache(ip);
+            const newRateLimit = await increamenttRateLimitCache(cacheKeyWithIp);
             if(newRateLimit && Number(newRateLimit) > rateLimit){
                 return res.status(429).json({
                     message: "Rate limit exceeded, please try again later"
